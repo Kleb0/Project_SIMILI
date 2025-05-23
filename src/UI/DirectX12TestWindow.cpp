16ecc61a5f2ee1d9c78c1f9cc6975d26a9e42437
Kleb0_UP <ID+username@users.noreply.github.com>
2025-05-23 02:02:06 +0200

    implementation of DirectX12 and automate building task, making the project more portable

---
+++ src/UI/DirectX12TestWindow.cpp
@@ -0,0 +1,56 @@
+#include "UI/DirectX12TestWindow.hpp"
+
+DirectX12TestWindow::DirectX12TestWindow()
+{
+    InitWindow();
+    renderer = std::make_unique<DirectX12Renderer>(hwnd);
+}
+
+DirectX12TestWindow::~DirectX12TestWindow()
+{
+    DestroyWindow(hwnd);
+}
+
+void DirectX12TestWindow::InitWindow()
+{
+    const wchar_t CLASS_NAME[] = L"DirectX12WindowClass";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    RegisterClass(&wc);
+
+    hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"DirectX 12 Preview",
+        WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        nullptr, nullptr, wc.hInstance, nullptr);
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+}
+
+void DirectX12TestWindow::Render()
+{
+    if (renderer && IsWindow(hwnd))
+    {
+        renderer->Render();
+    }
+}
+
+LRESULT CALLBACK DirectX12TestWindow::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
