fd2376da12707f8e1068953587513cbbc2cfe726
ClementMiermont <clementmi_artworkandgraph@outlook.com>
2025-08-04 18:57:18 +0200

    progress in the hierarchy inspector refactoring

---
+++ src/UI/HierarchyInspectorLogic/HandleHierarchyInteractions.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "WorldObjects/ThreeDObject.hpp"
+
+class HierarchyInspector;
+
+class HandleHierarchyInteractions {
+public:
+    HandleHierarchyInteractions(HierarchyInspector* inspector);
+
+    void clickOnSlot(bool& clickedOnItem, int index, ThreeDObject* obj);
+    void dragObject(ThreeDObject* obj, int index);
+    void dropOnSlot(ThreeDObject* obj, int index);
+    void dropOnObject(ThreeDObject* parent, ThreeDObject* child, int index);
+    void selectObject(ThreeDObject* obj);
+    void selectInList(ThreeDObject* obj);
+    void multipleSelection(ThreeDObject* obj);
+
+private:
+    HierarchyInspector* inspector;
+};
+
